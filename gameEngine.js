18f9d3fc4f5212bd9554582cadb7457d67529dd7
Ankit Verma <all.hail.ankit@gmail.com>
2019-02-16 20:44:56 -0800

    startgame state

--- gameEngine.js
+++ gameEngine.js
@@ -8,3 +8,11 @@ var clicked = [];
 var clicker = 0;
 var whose_turn = 0;
 var winner = "";
+
+// set the stage for the tic tac toe game of a lifetime
+function startgame(){
+	p1 = prompt('Ready player 1? What shall we call you?:');
+	p2 = prompt('Ready player 2? What shall we call you?:');
+	curr_player = p1;
+	document.getElementById("currentplayer").innerHTML = "your turn" + curr_player;
+}
