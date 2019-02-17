e38a7d7ced47613fd7ff5102a55a707cb40faa10
Ankit Verma <all.hail.ankit@gmail.com>
2019-02-16 20:22:38 -0800

    initialize files

---
+++ gameEngine.js
@@ -0,0 +1,10 @@
+// initialize game variables
+var p1 = "";
+var p2 = "";
+var curr_player = "";
+var XsAndOs = ["X", "O"];
+var scores = [0,0];
+var clicked = [];
+var clicker = 0;
+var whose_turn = 0;
+var winner = "";
